d16bc382dd5dffee82a3f52890c7b6ed9e328601
Mary Rossi <maryrossi106@gmail.com>
2022-09-07 21:07:45 -0500

    Fixed seed route

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require("express")
 const app = express()
 require("dotenv").config()
 const mongoose = require("mongoose")
-const Store = require("./models/products")
-const Seed = require("./models/storeSeed")
+const Product = require("./models/products")
+const storeSeed = require("./models/storeSeed")
 
 //Connect to MongoDB Atlas
 mongoose.connect(process.env.DATABASE_URL, {
@@ -22,19 +22,19 @@ db.on('disconnected', () => console.log('mongo disconnected'))
 //Middleware, body parser
 app.use(express.urlencoded({ extended: true }))
 
-// // Seed
-// app.get('/products/seed', (req, res) => {
-// 	Product.deleteMany({}, (error, allProducts) => {})
-// 	Product.create(storeSeed, (error, data) => {
-// 		res.redirect("/products")
-// 	})
-// })
+// Seed
+app.get('/products/seed', (req, res) => {
+	Product.deleteMany({}, (error, allProducts) => {})
+	Product.create(storeSeed, (error, data) => {
+		res.redirect("/products")
+	})
+})
 
 //Index
 app.get('/products', (req, res) => {
 	Product.find({}, (error, allProducts) => {
 		res.render("index.ejs", {
-			Product: allProducts,
+			products: allProducts,
 		});
 	});
 });
